f1f7e8a9722f5e79b937a2e5f0a8940d0b211967
sudachi0114 <jnevariste@gmail.com>
2022-07-22 09:27:25 +0900

    Stack のテストを追加

--- src/test/scala/com/github/sudachi0114/simpletest/FunSpecSpec.scala
+++ src/test/scala/com/github/sudachi0114/simpletest/FunSpecSpec.scala
@@ -2,12 +2,25 @@ package com.github.sudachi0114.simpletest
 
 import org.scalatest.funspec.AnyFunSpec
 
+import scala.collection.mutable
+import scala.collection.mutable.Stack
+
 class FunSpecSpec extends AnyFunSpec {
   describe("A Stack") {
 
-    it("should pop values in last-in-first-out order") (pending)
+    it("should pop values in last-in-first-out order") {
+      val stack = new Stack[Int]
+      stack.push(1)
+      stack.push(2)
+      assert(stack.pop() == 2)
+      assert(stack.pop() == 1)
+    }
 
-    it("should throw NoSuchElementException if an empty stack is poped") (pending)
+    it("should throw NoSuchElementException if an empty stack is poped") {
+      val emptyStack = new mutable.Stack[Int]
+      intercept[NoSuchElementException]{
+        emptyStack.pop()
+      }
+    }
   }
-
 }
