2975619ede6c7a53b20a7c5a679ddb6711ef494c
sudachi0114 <jnevariste@gmail.com>
2022-07-22 09:24:05 +0900

    FunSpec 公式ドキュメントで解説されているテストを追加

---
+++ src/test/scala/com/github/sudachi0114/simpletest/FunSpecSpec.scala
@@ -0,0 +1,13 @@
+package com.github.sudachi0114.simpletest
+
+import org.scalatest.funspec.AnyFunSpec
+
+class FunSpecSpec extends AnyFunSpec {
+  describe("A Stack") {
+
+    it("should pop values in last-in-first-out order") (pending)
+
+    it("should throw NoSuchElementException if an empty stack is poped") (pending)
+  }
+
+}
