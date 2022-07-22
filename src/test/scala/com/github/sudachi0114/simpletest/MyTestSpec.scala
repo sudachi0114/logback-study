b8a8ba62dd203ee165b714a3592c5eec3410d6c7
sudachi0114 <jnevariste@gmail.com>
2022-07-22 09:11:52 +0900

    FunSite -> FunSpec (某所でよく見る) に変更

--- src/test/scala/com/github/sudachi0114/simpletest/MyTestSpec.scala
+++ src/test/scala/com/github/sudachi0114/simpletest/MyTestSpec.scala
@@ -1,18 +1,21 @@
 package com.github.sudachi0114.simpletest
 
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.funspec.AnyFunSpec
 
-class MyTestSpec extends AnyFunSuite {
-  // test1
-  test("the name is set correctly in constructor") {
-    val p = new Person("hoge")
-    assert(p.name == "hoge")
-  }
+class MyTestSpec extends AnyFunSpec {
 
-  // test2
-  test("a Person's name can be changed") {
-    val p = new Person("foo")
-    p.name = "bar"
-    assert(p.name == "bar")
+  describe("Main test") {
+    // test1
+    it("should set correctly name in constructor") {
+      val p = new Person("hoge")
+      assert(p.name == "hoge")
+    }
+
+    // test2
+    it("should be able to change name Person") {
+      val p = new Person("foo")
+      p.name = "bar"
+      assert(p.name == "bar")
+    }
   }
 }
