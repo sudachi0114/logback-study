50d60ffea029664fe08e914d20b8fd8f98bf8277
sudachi0114 <jnevariste@gmail.com>
2022-07-25 10:19:53 +0900

    jackson-module で json を case class にパースするテストを追加

--- src/test/scala/com/github/sudachi0114/jackson/JacksonModuleSpec.scala
+++ src/test/scala/com/github/sudachi0114/jackson/JacksonModuleSpec.scala
@@ -1,10 +1,31 @@
 package com.github.sudachi0114.jackson
 
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-class JacksonModuleSpec extends AnyFunSpec {
+class JacksonModuleSpec extends AnyFunSpec with Matchers {
+
   describe("JacksonModuleSpec") {
-    // ここにテストを書く
-    it("should hoge") (pending)
+    it("should hoge") {
+      val json =
+        """|{"name":"some organization",
+           | "persons":
+           |  [{"name":"Taro","age":22},
+           |   {"name":"Hanako","age":18},
+           |   {"name":"Saburo","age":25}]}""".stripMargin
+       println(json)
+
+      val mapper = new ObjectMapper
+      mapper.registerModule(DefaultScalaModule)
+
+      val parsedJson = mapper.readValue(json, classOf[Organization])
+
+      val expectedPersons = List(Person("Taro", 22), Person("Hanako", 18), Person("Saburo", 25))
+      val expectedOrganization = Organization("some organization", expectedPersons)
+
+      parsedJson should be (expectedOrganization)
+    }
   }
 }
