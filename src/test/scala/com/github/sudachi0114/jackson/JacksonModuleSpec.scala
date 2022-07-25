5b5d293d8971f669dad15fa5174bfab85ccbb2ad
sudachi0114 <jnevariste@gmail.com>
2022-07-25 09:24:08 +0900

    JacksonModule のテストを追加

---
+++ src/test/scala/com/github/sudachi0114/jackson/JacksonModuleSpec.scala
@@ -0,0 +1,10 @@
+package com.github.sudachi0114.jackson
+
+import org.scalatest.funspec.AnyFunSpec
+
+class JacksonModuleSpec extends AnyFunSpec {
+  describe("JacksonModuleSpec") {
+    // ここにテストを書く
+    it("should hoge") (pending)
+  }
+}
